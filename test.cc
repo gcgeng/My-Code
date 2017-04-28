2249b673db226a212524dd556ff6f9dca5492597
Konjac <unrealgengchen@gmail.com>
2017-04-28 23:11:18 +0800

    ok

--- test.cc
+++ test.cc
@@ -1,6 +1,14 @@
 #include <cstdio>
 #include <queue>
 int main(int argc, char const *argv[]) {
-  printf("Hello world!");
-  return 0;
+	int a = 1, b = 2, ans = 0;
+	while(b <= 4000000) {
+		printf("%d ", a);
+		if(!(b&1)) ans += b;
+		int tmp = a;
+		a = b;
+		b += tmp;
+	}
+	printf("\n");
+	printf("%d", ans);
 }
